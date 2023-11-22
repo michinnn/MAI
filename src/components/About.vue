d934486d22952ff0c1c157e5bb389fc4aa46c62c
Dela Cruz, Michael Angelo <134262194+michinnn@users.noreply.github.com>
2023-11-22 13:12:24 +0800

    commit

--- src/components/About.vue
+++ src/components/About.vue
@@ -28,29 +28,29 @@ const skills = ref([
 <div>
 
 
-    <div class="pt-12 h-screen">
+    <div class="pt-12 lg:h-screen">
       <div class="flex justify-end items-center text-pinkish">
          <div class="absolute left-0 h-1.5 bg-pinkish w-[95vw]"></div>
          <div class="bg-white z-10">
           <h1 class="change-text lg:pr-32 lg:pl-4">ABOUT ME.</h1>
          </div>
       </div>
-      <div class="flex justify-between items-center px-32">
+      <div class="flex flex-col lg:flex-row justify-between items-center lg:px-32">
   
-        <div class="w-1/4 h-1/4 relative">
-          <img src="/me.jpg" alt="" class="rounded-full aspect-square absolute -bottom-8 -right-10">
-          <img src="/primo.png" alt="" class="spin absolute left-2 top-2 w-20 h-20">
-          <div class="w-80 h-80 border-2 border-pinkish rounded-full"></div>
+        <div class="lg:w-1/4 lg:h-1/4 relative">
+          <img src="/me.png" alt="" class="w-56 h-56 lg:w-80 lg:h-80 absolute -bottom-4 -right-5 lg:-bottom-8 lg:-right-10">
+          <img src="/primo.png" alt="" class="spin absolute left-2 top-2 w-12 h-12 lg:w-20 lg:h-20">
+          <div class="w-56 h-56 lg:w-80 lg:h-80 border-2 border-pinkish rounded-full"></div>
         </div>
-          <div class="flex flex-col justify-between w-1/2 relative">
+          <div class="flex flex-col justify-between pl-8 pr-8 pt-8 lg:w-1/2 lg:p-0 relative">
             <p class="font-medium text-black-300">Hello World! This is Ma. Andrhea Ivhon O. Sanchez an aspiring UI/UX Designer and Front-End Developer who is willing to enhance my skill and knowledge in my chosen field. Taking a Bachelor of Science in Information Technology Education major in Web and Mobile Application Development. I am currently on my On-The-Job training as UI designer trainee and Q.A trainee. Showing my creativeness and ideas in a blank canvas/screen is what I love to do. Just look through my website, enjoy and have a nice day!</p>
             <p class="font-bold italic pt-5 ">Visit at:</p>
             <div class="flex gap-2">
               <a href="https://www.linkedin.com/in/mai-sanchez/" target="_blank" class="text-pinkish font-semibold underline w-fit"><Icon icon="uil:linkedin"  class="w-7 h-7"/></a>
             <a href="https://www.behance.net/maisanchez" target="_blank" class="text-pinkish font-semibold underline w-fit"><Icon icon="fa-brands:behance-square" class="w-7 h-7"/></a>
-                <div class="absolute left-80 top-0">
-                  <div class="w-96 h-96 border-4 border-pinkish/30 rounded-full"></div>
-                  <div class="w-48 h-48 bg-pinkish/40 rounded-full -mt-20"></div>
+                <div class="absolute left-80 bottom-0 lg:left-80 lg:top-0">
+                  <div class="hidden lg:block lg:w-96 lg:h-96 border-4 border-pinkish/30 rounded-full"></div>
+                  <div class="hidden lg:block lg:w-48 lg:h-48 bg-pinkish/40 rounded-full -mt-20"></div>
                 </div>
            </div>
           
@@ -61,11 +61,13 @@ const skills = ref([
 
     <img src="/pink.png" alt="" class="w-full">
       <div class="pt-12 bg-pinkish">
-        <div class="flex justify-start items-center text-white px-32">
-          <div class="absolute right-0 h-1.5 bg-white w-[1100px]"></div>
-            <h1 class="flex change-text">SKILLS.</h1>
+        <div class="flex justify-start items-center text-white">
+          <div class="absolute right-0 h-1.5 bg-white w-[95vw]"></div>
+          <div class="bg-pinkish z-10">
+            <h1 class="change-text lg:pl-32 lg:pr-4">SKILLS.</h1>
+          </div>
         </div>
-         <div class="grid grid-cols-4 gap-20 px-32 py-10 w-full pb-20">
+         <div class="grid grid-cols-2 lg:grid-cols-4 gap-5 lg:gap-20 px-10 lg:px-32 py-10 w-full pb-20">
            <div v-for="(skill,index) in skills" :key="index" class="hover:-translate-y-10 transition-transform duration-300 ease-in-out delay-100 w-full relative bg-pinkish shadow-2xl rounded" target="_blank">
                 <div class="flex justify-center items-center pb-8">
                   <div style="background-repeat: no-repeat;
@@ -86,12 +88,14 @@ const skills = ref([
 
 
     <div class="pt-12 h-screen">
-      <div class="flex justify-end items-center text-pinkish px-32">
-        <div class="absolute left-0 h-1.5 bg-pinkish w-[850px]"></div>
-        <h1 class="flex change-text">CERTIFICATES.</h1>
+      <div class="flex justify-end items-center text-pinkish">
+        <div class="absolute left-0 h-1.5 bg-pinkish w-[95vw]"></div>
+        <div class="bg-white z-10">
+          <h1 class="change-text lg:pr-32 lg:pl-4">CERTIFICATES.</h1>
+        </div>
       </div>
-      <div class="flex justify-between items-center gap-5 px-32 py-10 w-full">
-        <div v-for="(certs,index) in certificates" :key="index" class="w-1/3 relative">
+      <div class="flex flex-col lg:flex-row justify-between items-center gap-5 px-10 lg:px-32 py-10 w-full">
+        <div v-for="(certs,index) in certificates" :key="index" class="lg:w-1/3 relative">
           <Image :src="certs.url" alt="certificate" preview 
           :pt="{ toolbar: ['absolute top-0 right-0 flex', 'p-8'],
                  closeicon: 'w-8 h-8',
@@ -122,6 +126,23 @@ const skills = ref([
   animation-name: fontChange;
   animation-duration: 5s;
   animation-iteration-count: infinite;
+}
+@media (max-width: 320px){
+  .change-text{
+    font-size: 30pt;
+  }
+}
+
+@media (max-width: 640px){
+  .change-text{
+    font-size: 50pt;
+  }
+}
+
+@media (max-width: 768px){
+  .change-text{
+    font-size: 60pt;
+  }
 }
 
 @keyframes fontChange {
